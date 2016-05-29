2c94af98faa61c7c0a87ca8ebfc0f3ee2c2ced1a
Michael Ayoul <michael.ayoul@gmail.com>
2016-05-28 22:54:15 -0300

    lightbox without jQuery.. raw JS

--- _js/lightbox.js
+++ _js/lightbox.js
@@ -1,48 +1,21 @@
-var lightbox_close_btn, lightbox_random, lightbox_setContent, lightbox_open, lightbox_close;
-$(document).ready(function() {
-    lightbox_close_btn = '<a href="javascript:void(0);" onclick="lightbox_close();" class="lb_close">Close</a>';
-    lightbox_random = function() {
-        var article = 'bouillabaisse';
-        var articles = $(".article");
-        if (articles.length) {
-            var randomIndex = Math.floor(Math.random() * articles.length);
-            article = $(articles[randomIndex]).find('a:first').attr('name');
-        }
-        //console.log('lightbox: '+article);
-        lightbox_setContent(article);
+var lightbox_close_btn = '<a href="javascript:void(0);" onclick="lightbox_close();" class="lb_close">Close</a>';
+var lightbox_random = function() {
+    var article;
+    var articles = document.getElementsByClassName("article");
+    if (articles.length) {
+        var randomIndex = Math.floor(Math.random() * articles.length);
+        article = articles[randomIndex];
+        var lightbox_content = document.getElementById('lb_content');
+        lightbox_content.innerHTML = lightbox_close_btn;
+        lightbox_content.innerHTML += article.innerHTML;
         lightbox_open();
-        //console.log('done - article: '+article);
-    };
-    lightbox_setContent = function(anchor) {
-        console.log('setContent - article: '+anchor);
-        var lightbox_content = $('#lb_content');
-        lightbox_content.empty();
-        var article = $("a[name='"+anchor+"']").parent().parent();
-        if (typeof article == "undefined") {
-            lightbox_random();
-            // console.log("error 404:"+anchor);
-            return;
-        }
-        lightbox_content.html(lightbox_close_btn);
-        lightbox_content.append(article.html());
-        lightbox_content.find('.back-to-top').remove();
-    };
-    lightbox_open = function() {
-        $('#lb_content').show();
-        $('#lb_overlay').show();
-    };
-    lightbox_close = function() {
-        $('#lb_content').hide();
-        $('#lb_overlay').hide();
-    };
-    $('.top-menu li a').click(function(e) {
-        e.preventDefault();
-        var anchor = $(this).attr('href');
-        lightbox_setContent(anchor.replace('#', ''));
-        lightbox_open();
-    });
-});
-$(document).keyup(function(e) {
-    if (e.keyCode === 27) // esc
-        lightbox_close();
-});
+    }
+};
+var lightbox_open = function() {
+    document.getElementById('lb_content').style.display = "inline-block";
+    document.getElementById('lb_overlay').style.display = "inline-block";
+};
+var lightbox_close = function() {
+    document.getElementById('lb_content').style.display = "none";
+    document.getElementById('lb_overlay').style.display = "none";
+};
